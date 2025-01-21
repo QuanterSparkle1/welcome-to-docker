6e0cd61d468c2bafda9277b34ca15e126f3925f5
Jinpeng Wu <jw6448@nyu.edu>
2025-01-21 17:47:16 -0500

    123

--- src/App.js
+++ src/App.js
@@ -1,59 +1,24 @@
 import "./App.css";
-import Confetti from "./Confetti";
+
 
 const shareMessage = "I just ran my first container using Docker";
 const shareLink = "https://docker.com/";
 
-const App = () => {
+function MyButton() {
   return (
-    <div className="App">
-      <Confetti />
-      <header className="App-header">
-        <h1 style={{ marginBottom: "0px" }}>Congratulations!!!</h1>
-        <p style={{ marginTop: "10px", marginBottom: "50px" }}>
-          You ran your first container.
-        </p>
-        <div>
-          <a
-            target="_blank"
-            href={
-              "https://twitter.com/intent/tweet?text=" +
-              shareMessage +
-              "&url=" +
-              shareLink
-            }
-            class="fa-brands fa-x-twitter"
-            rel="noopener noreferrer"
-          >
-            {" "}
-          </a>
-          <a
-            target="_blank"
-            href={
-              "https://www.linkedin.com/sharing/share-offsite/?url=" + shareLink
-            }
-            class="fa-brands fa-linkedin"
-            rel="noopener noreferrer"
-          >
-            {" "}
-          </a>
-          <a
-            target="_blank"
-            href={
-              "https://reddit.com/submit?title=" +
-              shareMessage +
-              "&url=" +
-              shareLink
-            }
-            class="fa-brands fa-reddit"
-            rel="noopener noreferrer"
-          >
-            {" "}
-          </a>
-        </div>
-      </header>
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
     </div>
   );
-};
+}
 
-export default App;
+
